343702557e1dcbc49f3e4d94982a0497b0170c3e
Alex6051 <81328208+Alex6051@users.noreply.github.com>
2024-09-13 02:13:46 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //может какой-то интерфейс тут потом сделать для настройки клиента и сервера?
+    return a.exec();
+}
